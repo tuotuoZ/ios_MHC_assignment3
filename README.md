247b228f7be49b7f4777c632c7681d1e254f5050
tuotuoZ <bz15@hampshire.edu>
2018-10-01 23:53:38 -0400

    16:03 till

--- README.md
+++ README.md
@@ -37,7 +37,7 @@ GIF created with [LiceCap](http://www.cockos.com/licecap/).
 
 ## Notes
 
-Describe any challenges encountered while building the app.
+1. What is the cliptobound?
 
 ## License
 
--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -11,22 +11,39 @@ import UIKit
 class BusinessCell: UITableViewCell {
     @IBOutlet weak var thumbImageView: UIImageView!
     
+   
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var ratingImageView: UIImageView!
     @IBOutlet weak var reviewsLabel: UILabel!
     @IBOutlet weak var catagoriesLabel: UILabel!
+    
     @IBOutlet weak var locationLabel: UILabel!
     
     var business: Business! {
         didSet {
             nameLabel.text = business.name
+            thumbImageView.setImageWith(business.imageURL!)
+            distanceLabel.text = business.distance
+            ratingImageView.image = business.ratingImage
+            reviewsLabel.text = "\(business.reviewCount!)Reviews"
+            catagoriesLabel.text = business.categories
         }
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        thumbImageView.layer.cornerRadius = 6
+        thumbImageView.clipsToBounds = true
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
